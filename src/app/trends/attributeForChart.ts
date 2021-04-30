f325aa2eb5de7354fc113ca899d545c7c95f474d
sidhu-12 <44739082+sidhu-12@users.noreply.github.com>
2021-04-30 11:02:40 +0530

    trends with date completed

---
+++ src/app/trends/attributeForChart.ts
@@ -0,0 +1,22 @@
+export const attributeListForChart =[
+{
+    attributeName : "Location",
+    value :"location",
+},
+{
+    attributeName : "Demand Id",
+    value :"demandId",
+},
+{
+    attributeName : "Skills",
+    value : "skills",
+},
+{
+    attributeName : "Background Check Status",
+    value :"backgroundCheckStatus",
+},
+{
+    attributeName : "Onboarding Status",
+    value :"onboardStatus",
+},
+]
---
+++ src/app/trends/chartData.ts
@@ -0,0 +1,4 @@
+export interface ChartData{
+    attributeName : string[],
+    count : number[]
+}
